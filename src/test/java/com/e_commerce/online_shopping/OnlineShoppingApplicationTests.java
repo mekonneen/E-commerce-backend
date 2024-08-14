5e8a1cebac081fada97471212059d03da8990612
mekonneen <mekonneen@gmail.com>
2024-08-13 21:35:54 -0400

    Initial Project Setup

---
+++ src/test/java/com/e_commerce/online_shopping/OnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.online_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
